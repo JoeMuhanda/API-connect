dcbbcd4055c66af49b07addb2c7d6e4911fedbf2
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-01-19 22:30:31 +0300

    Update Connect.js

--- Connect.js
+++ Connect.js
@@ -9,7 +9,7 @@ s1.src='https://embed.tawk.to/63a1e098b0d6371309d558fc/1gko5alvn';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
-})();
+
 </script>
 <!--End of Tawk.to Script-->
 
