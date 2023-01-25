7b740b308c91d48a6c0a03b47e5ecc3998612d7c
Joe Muhanda <88422453+JoeMuhanda@users.noreply.github.com>
2023-01-25 23:19:10 +0300

    Update Connect.js

--- Connect.js
+++ Connect.js
@@ -6,7 +6,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
 s1.src='https://embed.tawk.to/63a1e098b0d6371309d558fc/1gko5alvn';
-s1.charset='UTF-8';
+s1.charset='UTF-10';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
